4c15263ee16d80678782855080f13389691af93f
trinityhaisch <138032525+trinityhaisch@users.noreply.github.com>
2023-08-02 13:02:16 -0400

    Update beapp_add_row_generic_analysis_report.m adds diagnosis string

--- functions/beapp_add_row_generic_analysis_report.m
+++ functions/beapp_add_row_generic_analysis_report.m
@@ -18,21 +18,21 @@
 % ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 % The Batch Electroencephalography Automated Processing Platform (BEAPP)
 % Copyright (C) 2015, 2016, 2017
-% Authors: AR Levin, AS MÈndez Leal, LJ Gabard-Durnam, HM O'Leary
+% Authors: AR Levin, AS M√©ndez Leal, LJ Gabard-Durnam, HM O'Leary
 %
 % This software is being distributed with the hope that it will be useful,
 % but WITHOUT ANY WARRANTY; without even implied warranty of
 % MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See GNU General
 % Public License for more details.
 %
-% In no event shall Boston Childrenís Hospital (BCH), the BCH Department of
+% In no event shall Boston Children‚Äôs Hospital (BCH), the BCH Department of
 % Neurology, the Laboratories of Cognitive Neuroscience (LCN), or software
 % contributors to BEAPP be liable to any party for direct, indirect,
 % special, incidental, or consequential damages, including lost profits,
 % arising out of the use of this software and its documentation, even if
-% Boston Childrenís Hospital,the Laboratories of Cognitive Neuroscience,
+% Boston Children‚Äôs Hospital,the Laboratories of Cognitive Neuroscience,
 % and software contributors have been advised of the possibility of such
-% damage. Software and documentation is provided ìas is.î Boston Childrenís
+% damage. Software and documentation is provided ‚Äúas is.‚Äù Boston Children‚Äôs
 % Hospital, the Laboratories of Cognitive Neuroscience, and software
 % contributors are under no obligation to provide maintenance, support,
 % updates, enhancements, or modifications.
@@ -45,11 +45,12 @@
 % this program. If not, see <http://www.gnu.org/licenses/>.
 %~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-function [report_info,all_condition_labels,all_obsv_sizes] = beapp_add_row_generic_analysis_report(report_info,all_condition_labels,all_obsv_sizes,curr_file,file_proc_info,eeg_w)
+function [report_info,all_condition_labels,all_obsv_sizes] = beapp_add_row_generic_analysis_report(report_info,all_condition_labels,all_obsv_sizes,curr_file,file_proc_info,eeg_w,diagnosis_map)
 if isfield(file_proc_info,'evt_conditions_being_analyzed')
     all_condition_labels(curr_file,:)=file_proc_info.grp_wide_possible_cond_names_at_segmentation';
 end
 
+report_info.Diagnosis(curr_file)= convertCharsToStrings(diagnosis_map{([diagnosis_map{:,1}]==file_proc_info.diagnosis),2}); %TH
 report_info.Src_Net_Type(curr_file) = file_proc_info.net_typ;
 report_info.Src_SRate(curr_file) = file_proc_info.src_srate;
 report_info.Current_SRate(curr_file) = file_proc_info.beapp_srate;
